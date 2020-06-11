72b5504b4d773fc9e01e1d0c1b39c435e3c768e3
Frankie Foston <frankie.foston@spire.com>
2020-06-11 13:58:29 +0100

    Derive debug for StateVector and add a test case for negative prop

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,7 @@ pub enum Error {
 
 type Result<T> = std::result::Result<T, Error>;
 
+#[derive(Debug)]
 pub struct StateVector {
     pub position: [f64; 3],
     pub velocity: [f64; 3],
@@ -90,6 +91,9 @@ impl TwoLineElement {
 
     pub fn propagate_to(&self, t: DateTime<Utc>) -> Result<StateVector> {
         let tle_epoch = self.elements.epoch();
+        // TODO: determine correct behaviour for negative prop
+        // assert!(t >= tle_epoch);
+
         let min_since_epoch = (t - tle_epoch).num_days() as f64;
 
         let (r, v) = sgp4_sys::run_sgp4(
@@ -122,6 +126,20 @@ mod tests {
 
         let _s1 = tle.propagate_to(epoch);
         let _s2 = tle.propagate_to(epoch + Duration::hours(1));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_negative_time_propagation() -> Result<()> {
+        let line1 = "1 25544U 98067A   20148.21301450  .00001715  00000-0  38778-4 0  9992";
+        let line2 = "2 25544  51.6435  92.2789 0002570 358.0648 144.9972 15.49396855228767";
+
+        let tle = TwoLineElement::new(line1, line2)?;
+        let epoch = tle.epoch()?;
+
+        let _s1 = tle.propagate_to(epoch);
+        let _s2 = tle.propagate_to(epoch - Duration::days(30));
 
         Ok(())
     }
