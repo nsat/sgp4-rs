5ba6df79f36324b113bc335689395d22125a031e
Josh Singer <josh.singer@spire.com>
2023-07-19 13:19:58 +0100

    changed vec to array in response to clippy

--- src/tlegen.rs
+++ src/tlegen.rs
@@ -1,5 +1,5 @@
 use argmin::{
-    core::{CostFunction, Executor},
+    core::{CostFunction, Executor, observers::{SlogLogger, ObserverMode}},
     solver::neldermead::NelderMead,
 };
 use chrono::{DateTime, Datelike, Timelike, Utc};
@@ -73,12 +73,16 @@ impl StateVector {
             // initial simplex requires n+1 vertices
             initial_simplex.push(init_param.clone());
         }
-        let perturbations = vec![0.1, 0.1, 0.01, 1.0, 5.0, 1.0];
+        let perturbations = [0.1, 0.1, 0.01, 1.0, 1.0, 1.0];
         for i in 0..6 {
             // use a custom offset for each parameter
             initial_simplex[i][i] += perturbations[i];
         }
-        let solver: NelderMead<Vec<f64>, f64> = NelderMead::new(initial_simplex);
+        let solver: NelderMead<Vec<f64>, f64> = NelderMead::new(initial_simplex)
+        .with_alpha(0.9).expect("error")
+        .with_gamma(1.1).expect("error")
+        .with_rho(0.25).expect("error")
+        .with_sigma(0.1).expect("error");
         let res = Executor::new(cost, solver)
             .configure(|state| state.param(init_param).max_iters(1000).target_cost(0.0))
             //.add_observer(SlogLogger::term(), ObserverMode::Always)
@@ -126,11 +130,11 @@ fn tle_line_2(
 ) -> String {
     use std::f64::consts::PI;
 
-    let incl = inclination.get::<angle::degree>();
-    let raan = raan.get::<angle::degree>();
+    let incl = normalize_angle(inclination).get::<angle::degree>();
+    let raan = normalize_angle(raan).get::<angle::degree>();
     let ecc_int = (eccentricity * 10e6).round() as i64;
-    let argp = argument_of_perigee.get::<angle::degree>();
-    let ma = mean_anomaly.get::<angle::degree>();
+    let argp = normalize_angle(argument_of_perigee).get::<angle::degree>();
+    let ma = normalize_angle(mean_anomaly).get::<angle::degree>();
     let consts = sgp4_sys::gravitational_constants();
     let mm = SECONDS_PER_DAY
         / ((2.0 * PI) * (semimajor_axis.get::<kilometer>().powi(3) / consts.mu).sqrt());
@@ -245,7 +249,7 @@ mod tests {
         let v_1 = [5.087843659697572, -3.2858873951805836, 4.561428718239809];
         let svector = StateVector::new(epoch, r_1, v_1);
         let tle_string = svector.as_tle_at(0, epoch).unwrap();
-        println!("tle_string:\n{:?}", tle_string);
+        println!("tle_string:\n{}", tle_string);
         let svector_2 = TwoLineElement::from_lines(&tle_string)?.propagate_to(epoch)?;
         let r_2 = svector_2.position;
         let v_2 = svector_2.velocity;
@@ -259,6 +263,29 @@ mod tests {
         assert_approx_eq!(f64, v_1[0], v_2[0], epsilon = 0.01);
         assert_approx_eq!(f64, v_1[1], v_2[1], epsilon = 0.01);
         assert_approx_eq!(f64, v_1[2], v_2[2], epsilon = 0.01);
+        Ok(())
+    }
+
+
+    #[test]
+    fn test_roundtrip_tle_to_tle() -> Result<()> {
+        let epoch = Utc.with_ymd_and_hms(2021, 5, 25, 0, 0, 0).unwrap();
+        let tle_1 = "1 00000U 21001A   21145.00000000  .00000000  00000-0  00000-0 0  9997\n2 00000  36.9006 237.1418 0013279   1.4043 318.6732 14.97334669000013".to_string();
+        println!("tle_1:\n{}", tle_1);
+        let svector = TwoLineElement::from_lines(&tle_1)?.propagate_to(epoch)?;
+        let tle_2 = svector.as_tle_at(0, epoch).unwrap();
+        println!("tle_2:\n{}", tle_2);
+        Ok(())
+    }
+
+    #[test]
+    fn test_roundtrip_tle_to_tle_2() -> Result<()> {
+        let epoch = Utc.with_ymd_and_hms(2021, 5, 25, 0, 0, 0).unwrap();
+        let tle_1 = "1 00000U 21001A   21145.00000000  .00000000  00000-0  00000-0 0  9997\n2 00000  36.9144 237.1225 1121181   3.5239 316.6354 14.96118753000010".to_string();
+        println!("tle_1:\n{}", tle_1);
+        let svector = TwoLineElement::from_lines(&tle_1)?.propagate_to(epoch)?;
+        let tle_2 = svector.as_tle_at(0, epoch).unwrap();
+        println!("tle_2:\n{}", tle_2);
         Ok(())
     }
 }
